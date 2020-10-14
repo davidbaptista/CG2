f4ef81b9515412d90f2f8f0a557fe7635c8d9e0c
JAntunes22 <jpc.antunes22@gmail.com>
2020-10-14 15:40:29 +0100

    mesa

--- src/js/script.js
+++ src/js/script.js
@@ -4,60 +4,101 @@ let camera, scene, renderer;
 
 let geometry, material, mesh;
 
-let ball;
+let table, s1, s2, s3, s4, s5, s6;
 
-function addTableLeg(obj, x, y, z) {
+function createBox(l, h, w) {
     'use strict';
 
-    geometry = new THREE.CubeGeometry(2, 6, 2);
+    geometry = new THREE.CubeGeometry(l, h, w);
+    material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y - 3, z);
-    obj.add(mesh);
+    mesh.position.set(0, 0, 0);
+    return mesh;
 }
 
-function addTableTop(obj, x, y, z) {
-    'use strict';
-    geometry = new THREE.CubeGeometry(60, 2, 20);
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
-    obj.add(mesh);
+function createCylinder(l) {
+    'use strict'
+
+	geometry = new THREE.CylinderGeometry(1, 3, l, 64);
+    material = new THREE.MeshBasicMaterial({color: 0xffff00, wireframe: true});
+    mesh = new THREE.Mesh(geometry, material);	
+	mesh.position.set(0, 0, 0);
+	return mesh;
 }
 
-function createBall(x, y, z) {
+function createTable(l, h ,w) {
     'use strict';
 
-    ball = new THREE.Object3D();
+    table = new THREE.Object3D();
+    let leg_size = 40;
+    let wall_width = 6;
 
-    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false });
-    geometry = new THREE.SphereGeometry(4, 10, 10);
-    mesh = new THREE.Mesh(geometry, material);
+    let table_top = createBox(l, h, w);
+    let leg1 = createBox(l/leg_size, l/4, l/leg_size);
+    let leg2 = createBox(l/leg_size, l/4, l/leg_size);
+    let leg3 = createBox(l/leg_size, l/4, l/leg_size);
+    let leg4 = createBox(l/leg_size, l/4, l/leg_size);
+    let wall1 = createBox(l, 12, wall_width);
+    let wall2 = createBox(l, 12, wall_width);
+    let wall3 = createBox(wall_width, 12, w);
+    let wall4 = createBox(wall_width, 12, w);
 
-    ball.add(mesh);
-    ball.position.set(x, y, z);
+    leg1.position.set(l/2-l/(leg_size*2), -l/8, w/2-l/(leg_size*2));
+    leg2.position.set(-l/2+l/(leg_size*2), -l/8, w/2-l/(leg_size*2));
+    leg3.position.set(l/2-l/(leg_size*2), -l/8, -w/2+l/(leg_size*2));
+    leg4.position.set(-l/2+l/(leg_size*2), -l/8, -w/2+l/(leg_size*2));
+    wall1.position.set(0, 0, w/2-(wall_width/2));
+    wall2.position.set(0, 0, -w/2+(wall_width/2));
+    wall3.position.set(l/2-(wall_width/2), 0, 0);
+    wall4.position.set(-l/2+(wall_width/2), 0, 0);
 
-    scene.add(ball);
-}
-
-
-function createTable(x, y, z) {
-    'use strict';
-
-    var table = new THREE.Object3D();
-
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: false });
-
-    addTableTop(table, 0, 0, 0);
-    addTableLeg(table, -25, -1, -8);
-    addTableLeg(table, -25, -1, 8);
-    addTableLeg(table, 25, -1, 8);
-    addTableLeg(table, 25, -1, -8);
+    table.add(table_top);
+    table.add(leg1);
+    table.add(leg2);
+    table.add(leg3);
+    table.add(leg4);
+    table.add(wall1);
+    table.add(wall2);
+    table.add(wall3);
+    table.add(wall4);
 
     scene.add(table);
 
-    table.position.x = x;
-    table.position.y = y;
-    table.position.z = z;
+    //add sticks to the table
+
+    s1 = new THREE.Object3D();
+    let s = createCylinder(w);
+
+    s1.add(s)
+
+    s2 = new THREE.Object3D();
+    s = createCylinder(w);
+    s2.add(s)
+
+    s3 = new THREE.Object3D();
+    s = createCylinder(w);
+    s3.add(s)
+
+    s4 = new THREE.Object3D();
+    s = createCylinder(w);
+    s4.add(s)
+
+    s5 = new THREE.Object3D();
+    s = createCylinder(w);
+    s5.add(s)
+
+    s6 = new THREE.Object3D();
+    s = createCylinder(w);
+    s6.add(s)
+
+    scene.add(s1);
+    scene.add(s2);
+    scene.add(s3);
+    scene.add(s4);
+    scene.add(s5);
+    scene.add(s6);
 }
+
 
 function createScene() {
     'use strict';
@@ -67,16 +108,15 @@ function createScene() {
 
     scene.add(new THREE.AxisHelper(10));
 
-    createTable(0, 8, 0);
-    createBall(0, 0, 15);
+    createTable(245, 5, 135);
 }
 
 function createCamera() {
     'use strict';
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.x = 50;
-    camera.position.y = 50;
-    camera.position.z = 50;
+    camera.position.x = 200;
+    camera.position.y = 200;
+    camera.position.z = 200;
     camera.lookAt(scene.position);
 }
 
